b9561991cf081d6e394040d8bd4b67319157aa62
Earl Potters <earl.potters@gmail.com>
2022-11-18 15:15:32 +0100

    prettier fix

--- pages/api/getImage.js
+++ pages/api/getImage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default  function handler(req, res) {
+export default function handler(req, res) {
   console.log("req", req.body.data); //this is your pdf to get sent
   //fetch data via get request to SlyRacoon's GPU run pod (see endpoint documentation)
 
@@ -8,31 +8,31 @@ export default  function handler(req, res) {
   // upload to nft.storage
 
   let data = JSON.stringify({
-    "prompt": "Elf in a forest",
-    "steps": 50,
-    "sampler_index": "DDIM"
+    prompt: "Elf in a forest",
+    steps: 50,
+    sampler_index: "DDIM",
   });
-  
+
   let config = {
-    method: 'post',
-    url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
-    headers: { 
-      'Content-Type': 'application/json'
+    method: "post",
+    url: "http://127.0.0.1:7860/sdapi/v1/txt2img",
+    headers: {
+      "Content-Type": "application/json",
     },
-    data : data
+    data: data,
   };
 
   axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-    res.status(200).json({
-      imageUrl: "data:image/png;base64," + response.data["images"][0],
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.status(200).json({
+        imageUrl: "data:image/png;base64," + response.data["images"][0],
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "Error generating image",
+      });
     });
-  })
-.catch(function (error) {
-  console.log(error);
-  res.status(500).json({
-    error: "Error generating image",
-  });
-});
 }
