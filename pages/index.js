2dab9921357422a3a6b78d02c4f69c5a2f568f3c
tippi fifestarr <tippi@boober.org>
2022-11-18 10:43:29 -0600

    fixed runites PR, integrated his css

--- pages/index.js
+++ pages/index.js
@@ -43,32 +43,48 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <h1>The Thing:</h1>
+    <div id="bodytype">
+      <h1>DnDnDiffusion</h1>
       {/* input takes in text for now, but eventually allows a .pdf upload */}
       {/* <form method="post"> */}
-      <input
-        onChange={(e) => setValue(e)}
-        type="text"
-        name="race"
-        placeholder="Enter your race"
-      />
-      <input
-        onChange={(e) => setValue(e)}
-        type="text"
-        name="class"
-        placeholder="Enter your class"
-      />
-      <button onClick={send} type="submit" name="submit">
-        {imageProcessing ? "Waiting for image..." : "Send"}
-      </button>
 
-      {imageResult && (
-        <>
-          <img src={imageResult.imageUrl} alt={"its your image!"} />
-          <div onClick={storeImage}>NFT.Storage</div>
-        </>
-      )}
+      <div id="inputs">
+        <input
+          onChange={(e) => setValue(e)}
+          type="text"
+          name="race"
+          placeholder="Enter your race"
+        />
+        <input
+          onChange={(e) => setValue(e)}
+          type="text"
+          name="class"
+          placeholder="Enter your class"
+        />
+        <select id="gender" name="gender">
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Non-Binary">Non-Binary</option>
+        </select>
+
+        <button onClick={send} type="submit" name="submit">
+          {imageProcessing ? "Waiting for image..." : "Send"}
+        </button>
+      </div>
+      <br></br>
+
+      <div id="imagegeneration">
+        {imageResult && (
+          <>
+            <img src={imageResult.imageUrl} alt={"its your image!"} />
+            <button onClick={storeImage}>NFT.Storage</button>
+          </>
+        )}
+      </div>
+      <div id="inputs">
+        <button type="button">Enhance!</button>
+        <button type="button">Save as NFT</button>
+      </div>
     </div>
   );
 }
