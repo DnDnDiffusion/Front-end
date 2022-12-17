a8ccb37275fbfd1378e2922d595f7bc9aa20db70
somya-15 <somyatomar15@gmail.com>
2022-12-18 00:34:59 +0530

    Somya's GIF attempt

---
+++ .history/pages/index_20221218000533.js
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState({
+    race: "",
+    class: "",
+  });
+  const [imageProcessing, setImageProcessing] = useState(false); //processing state ie. loading...
+  const [error, setError] = useState(null); //error msg
+  const [imageResult, setImageResult] = useState(null); //url
+
+  const setValue = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const send = async () => {
+    setImageProcessing(true);
+    console.log("data: ", data);
+    //send req to backend with data to get the goods
+    let result = await fetch("/api/getImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        data: data,
+      }),
+    }); //result is given as a URL in S3 bucket or smth
+    result = await result.json();
+    if (result.error) setError(result.error);
+    console.log("result from image processing", result);
+    setImageResult(result);
+    setImageProcessing(false);
+  };
+
+  return (
+    <div>
+      <h1>The Thing:</h1>
+      {/* input takes in text for now, but eventually allows a .pdf upload */}
+      {/* <form method="post"> */}
+      <input
+        onChange={(e) => setValue(e)}
+        type="text"
+        name="race"
+        placeholder="Enter your race"
+      />
+      <input
+        onChange={(e) => setValue(e)}
+        type="text"
+        name="class"
+        placeholder="Enter your class"
+      />
+      <button onClick={send} type="submit" name="submit">
+        {imageProcessing ? "Waiting for image..." : "Send"}
+      </button>
+
+      {imageResult && (
+        <img src={imageResult.imageUrl} alt={"its your image!"} />
+      )}
+    </div>
+  );
+}
