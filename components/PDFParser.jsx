5663fa5627d00821851a150e574f22fe0867aa06
danj-O <danjo.develop@gmail.com>
2022-12-17 22:09:59 -0500

    fixes

--- components/PDFParser.jsx
+++ components/PDFParser.jsx
@@ -30,25 +30,26 @@ export default function PDFParser() {
   };
 
   return (
-    <div className="flex items-center gap-4 flex-col">
+    <div className="flex items-center gap-4 flex-col bg-black">
       <div className="flex flex-col w-fit gap-2">
         <input onChange={handlePDFChange} type="file" className="border-2 rounded-lg p-4 bg-yellow-500 bg-opacity-50" />
         <button className=" bg-emerald-800 text-emerald-200 p-4 rounded-lg" onClick={handleUpload}>
           Upload PDF
         </button>
       </div>
-      <div className="flex gap-2 flex-wrap justify-center items-center max-w-xl h-28">
+      <div className="flex gap-2 flex-wrap justify-center items-center max-w-4xl h-28">
         {
           //if pdfData exists, loop through object and display key and value
           pdfData &&
             Object.entries(pdfData).map(([key, value]) => {
+              console.log(typeof value);
               return (
                 <div
                   key={key}
-                  className=" bg-violet-800 bg-opacity-50 border-2 rounded-lg p-6 h-full flex flex-col justify-center items-center"
+                  className=" bg-violet-800 bg-opacity-50 border-2 rounded-lg p-6 h-full flex flex-col justify-center items-center min-w-[200px]"
                 >
                   <p>{key}</p>
-                  <p>{value}</p>
+                  <p>{value instanceof Array ? `${value[0]} : ${value[1]}` : value}</p>
                 </div>
               );
             })
