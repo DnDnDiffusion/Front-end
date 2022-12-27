d6fed386d277968087eae50c3a4f32604c303723
airecr <aire.forwork@gmail.com>
2022-12-28 00:09:08 +0700

    Adding the last button

--- components/Midpage.jsx
+++ components/Midpage.jsx
@@ -21,12 +21,12 @@ export default function Content() {
                 <div className="flex flex-col justify-center align-center items-center bg-[#110402] mr-7 p-6 w-1/3">
                     <p>Fine Tuned AI Models and parsing</p>
                     <p>algorithms generate data</p>
-                    <img src="" alt="icons" className="w-40 h-40 object-contain pt-5" />
+                    <img src="" alt="icon2" className="w-40 h-40 object-contain pt-5" />
                 </div>
                 <div className="flex flex-col justify-center align-center items-center bg-[#110402] p-6 w-1/3">
                     <p>Reveive 3, 4 or 9 avatar images</p>
                     <p>based on your character sheet</p>
-                    <img src="" alt="icons" className="w-40 h-40 object-contain pt-5" />
+                    <img src="" alt="icon3" className="w-40 h-40 object-contain pt-5" />
                 </div>
             </div>
 
@@ -38,6 +38,32 @@ export default function Content() {
                 EXPLORE</button>
             </div>
             
+            {/* Intruction Cards */}
+            <div className="flex flex-row mt-8 mr-12 ml-12">
+                <div className="flex flex-col justify-center align-center items-center bg-[#110402] mr-7 p-6 w-1/3">
+                    <p>Mint an ERC721</p>
+                    <p>CNFT Avatar</p>
+                    <img src="" alt="icon1" className="w-40 h-40 object-contain pt-5" />
+                </div>
+                <div className="flex flex-col justify-center align-center items-center bg-[#110402] mr-7 p-6 w-1/3">
+                    <p>Join the community</p>
+                    <img src="" alt="icon2" className="w-40 h-40 object-contain pt-5" />
+                </div>
+                <div className="flex flex-col justify-center align-center items-center bg-[#110402] p-6 w-1/3">
+                    <p>Buy interactive</p>
+                    <p>dice to get gold</p>
+                    <img src="" alt="icon3" className="w-40 h-40 object-contain pt-5" />
+                </div>
+            </div>
+
+            {/* Go Premium Button */}
+            <div className="flex justify-center mt-8 mb-8">
+                <button className="  
+                    rounded-full bg-[#CA8A04] text-black pr-10 pl-10 pt-2 pb-2
+                    border-2 border-black text-2xl">
+                GO PREMIUM</button>
+            </div>
+                        
         </div>
     )
 }
