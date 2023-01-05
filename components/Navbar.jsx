12d655c7459e8c7ea212e7d832e8ef0316ce32ae
tippi fifestarr <tippi@boober.org>
2023-01-05 15:28:22 -0600

    hamburger breakpoints

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,12 +7,12 @@ export default function Navbar() {
     <nav className="flex justify-start justify-between items-center p-6 container mx-auto">
       <Image src={Logo} alt="logo" className="shrink w-12 object-contain" />
       {/* hamburger menu, has no functionality */}
-      <div id="hamburger" className="hover:cursor-pointer sm:hidden">
+      <div id="hamburger" className="hover:cursor-pointer md:hidden">
         <span className="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"></span>
         <span className="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"></span>
         <span className="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"></span>
       </div>
-      <ul className="sm:flex text-center hidden justify-between items-center p-9">
+      <ul className="md:flex text-center hidden justify-between items-center p-9">
         <li className="text-yellow-500 mr-5 hover:text-red-500 transition-all">
           <a href="#create">CREATE</a>
         </li>
