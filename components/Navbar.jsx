91716ebb8bc39835fd7b47dadfb2d5e3f4d61b65
tippi fifestarr <tippi@boober.org>
2023-01-05 11:54:46 -0600

    navbar resize

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="flex justify-start justify-between items-center p-6 container mx-auto">
-      <Image src={Logo} alt="logo" className="shrink w-1/6 object-contain" />
+      <Image src={Logo} alt="logo" className="shrink w-12 object-contain" />
       {/* hamburger menu, has no functionality */}
       <div id="hamburger" className="hover:cursor-pointer sm:hidden">
         <span className="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"></span>
@@ -13,19 +13,19 @@ export default function Navbar() {
         <span className="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"></span>
       </div>
       <ul className="sm:flex text-center hidden justify-between items-center p-9">
-        <li className="text-yellow-500 mr-5 hover:text-red-500 hover:text-lg transition-all">
+        <li className="text-yellow-500 mr-5 hover:text-red-500 transition-all">
           <a href="#create">CREATE</a>
         </li>
-        <li className="mx-5 hover:text-yellow-500 hover:text-lg transition-all">
+        <li className="mx-5 hover:text-yellow-500 transition-all">
           <a href="#explore">EXPLORE</a>
         </li>
-        <li className="mx-5 hover:text-yellow-500 hover:text-lg transition-all">
+        <li className="mx-5 hover:text-yellow-500 transition-all">
           <a href="#gmdice">GM.DICE</a>
         </li>
-        <li className="mx-5 hover:text-yellow-500 hover:text-lg transition-all">
+        <li className="mx-5 hover:text-yellow-500 transition-all">
           <a href="#about">ABOUT</a>
         </li>
-        <li className="ml-5 hover:text-yellow-500 hover:text-lg transition-all">
+        <li className="ml-5 hover:text-yellow-500 transition-all">
           <a href="#contact">CONTACT</a>
         </li>
       </ul>
