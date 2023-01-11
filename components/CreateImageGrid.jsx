0f144de87ba48042b3b2441e66f845443d7fff38
danj-O <danjo.develop@gmail.com>
2023-01-11 18:02:46 -0500

    fixed nextimage issue

--- components/CreateImageGrid.jsx
+++ components/CreateImageGrid.jsx
@@ -1,28 +1,22 @@
-import Image from "next/image";
-import React from "react";
-import { useEffect } from "react";
-import { LoadingTips } from "./LoadingTips";
-import Placeholder from "../public/images/CREATE/placeholder.png";
+import Image from "next/image"
+import React from "react"
+import { useEffect } from "react"
+import { LoadingTips } from "./LoadingTips"
+import Placeholder from "../public/images/CREATE/placeholder.png"
 
-export const CreateImageGrid = ({
-  imageProcessing,
-  imageResult,
-  error,
-  pdfData,
-  setSelectedImage,
-}) => {
+export const CreateImageGrid = ({ imageProcessing, imageResult, error, pdfData, setSelectedImage }) => {
   useEffect(() => {
-    if (imageProcessing) console.log("imageProcessing: ", imageProcessing);
+    if (imageProcessing) console.log("imageProcessing: ", imageProcessing)
     // console.log("imageProcessing: ", imageProcessing);
     // console.log("imageResult: ", imageResult);
-  }, [imageProcessing, imageResult]);
+  }, [imageProcessing, imageResult])
 
   if (error)
     return (
       <div className="flex justify-center items-center h-full text-3xl">
         <p className=" text-red-400">{error}</p>
       </div>
-    );
+    )
   if (imageProcessing)
     //TODO: RENDER TIPS
     return (
@@ -35,13 +29,13 @@ export const CreateImageGrid = ({
           height={256}
         /> */}
       </div>
-    );
+    )
   if (!pdfData)
     return (
       <div className="flex justify-center items-center h-full text-3xl">
         <p className=" text-yellow-400 ">Please upload a character sheet!</p>
       </div>
-    );
+    )
   else if (imageResult)
     return (
       <div className="flex justify-center items-center">
@@ -53,19 +47,19 @@ export const CreateImageGrid = ({
               onClick={() => setSelectedImage(image)}
               src={image}
               alt=""
+              width={128}
+              height={128}
             />
-          );
+          )
         })}
       </div>
-    );
+    )
   else if (pdfData) {
     return (
       <div className="flex justify-center items-center h-full text-3xl">
-        <p className=" text-emerald-400">
-          We have your character sheet parsed, you can now generate your image!
-        </p>
+        <p className=" text-emerald-400">We have your character sheet parsed, you can now generate your image!</p>
       </div>
-    );
+    )
   }
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
@@ -97,5 +91,5 @@ export const CreateImageGrid = ({
         <Image src={Placeholder} alt="" />
       </div>
     </div>
-  );
-};
+  )
+}
