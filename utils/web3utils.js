a9247c29d10418c12f2c03ccb06e7ec3fdcfd0b7
danj-O <danjo.develop@gmail.com>
2023-01-15 11:48:47 -0500

    metadata + minting

--- utils/web3utils.js
+++ utils/web3utils.js
@@ -5,19 +5,37 @@ const token = process.env.NEXT_PUBLIC_NFT_STORAGE
 // takes in base64 binary image data
 async function avatarNFTSTORAGE(someBinaryImageData, prompt, pdfData) {
   const client = new NFTStorage({ token: token })
-  const uploadObject = createStructuredMetadata(pdfData, prompt, someBinaryImageData)
+  const uploadObject = await createStructuredMetadata(pdfData, prompt, someBinaryImageData)
   const metadata = await client.store(uploadObject)
   return metadata.url
 }
 
-function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
+const base64EncodedImage = async (base64ImageString) => {
+  const fetchImage = await fetch(base64ImageString)
+  console.log("fetchImage", fetchImage)
+  const blob = await fetchImage.blob()
+  const file = new File([blob], "something.png", { type: "image/png" })
+  return file
+}
+
+async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
+  // const imageFile = new File([someBinaryImageData], "diffused.png", {
+  //   type: "image/png",
+  // })
+
+  //make binary image data into blob
+  // const blob = new Blob([someBinaryImageData], { type: "image/png" })
+  // console.log("imageFile", blob)
+
+  const imageFile = await base64EncodedImage(someBinaryImageData)
+
+  console.log("imageFile", imageFile)
+
   const structuredMetadata = {
     name: "Wizard NFT!",
     description:
       "This is a wizard NFT, created during 'Operation Dragonborn' by our fearless heroes and the Scope Creeper! Just try to funge it. You can't do it.",
-    image: new File([someBinaryImageData], "diffused.png", {
-      type: "image/png",
-    }),
+    image: imageFile,
     external_url: "https://operation-dragonborn.vercel.app/",
     attributes: [
       {
