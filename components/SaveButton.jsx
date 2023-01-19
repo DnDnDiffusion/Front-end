dca58c246640a19048551841fa16d3cb827ed602
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-01-19 14:39:55 -0500

    styled save/download button

--- components/SaveButton.jsx
+++ components/SaveButton.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+
 const SaveButton = (imageResult) => {
     return (
         <>
-            <a href={imageResult.imageResult.images[0]} download="character_image">SAVE BUTTON
+            <a href={imageResult.imageResult.images[0]} download="character_image" className="save-button flex flex-col text-black p-12">
+                <p className="flex justify-center text-xl mb-2">SAVE</p>
+                <div className="flex justify-center content-center">
+                    <div className="flex justify-center align-center mr-2">
+                        <Image
+                            src="/images/SaveButton/download.svg"
+                            alt="button-image"
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                    <p className="text-md">PNG</p>
+                </div>
+
             </a>
 
         </>
